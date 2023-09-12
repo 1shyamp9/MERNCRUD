0273ba1796460ab465e818d93f38ac983f96c7a7
1shyamp9 <1shyamp9@gmail.com>
2023-09-13 01:22:29 +0530

    Cors 1

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ app.use(cookieParser())
 
 app.use(userRoute);
 
-// app.use(cors({ 
-//     origin:[process.env.FRONTEND_URL],
-//     methods:["GET","POST","PUT","DELETE"],
-//     credentials:true,
-// }))
+app.use(cors({ 
+    origin:[process.env.FRONTEND_URL],
+    methods:["GET","POST","PUT","DELETE"],
+    credentials:true,
+}))
