274ff6cf0f3d798faa0369d228ed632272cafe2b
1shyamp9 <1shyamp9@gmail.com>
2023-09-13 10:00:08 +0530

    Cors Done

--- app.js
+++ app.js
@@ -10,11 +10,9 @@ configDotenv({
 })
 app.use(express.json());
 app.use(cookieParser())
-
-app.use(userRoute);
-
 app.use(cors({ 
     origin:[process.env.FRONTEND_URL],
     methods:["GET","POST","PUT","DELETE"],
     credentials:true,
 }))
+app.use(userRoute);
